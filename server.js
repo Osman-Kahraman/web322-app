2f5abd7618ad62c2b3dca2b4cd80fd19110a8915
Osman Kahraman <65239658+Osman-Kahraman@users.noreply.github.com>
2025-02-26 19:03:20 -0500

    modified for assingment 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ I declare that this assignment is my own work in accordance with Seneca Academic
 been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 Name: Osman KAHRAMAN
 Student ID: 172781221
-Date: 04/02/2025
+Date: 026/02/2025
 Cyclic Web App URL: https://replit.com/@okahraman2/web322-app
 https://replit.com/@okahraman2/web322-app?v=1
 GitHub Repository URL: https://github.com/Osman-Kahraman/web322-app
@@ -13,9 +13,21 @@ GitHub Repository URL: https://github.com/Osman-Kahraman/web322-app
 const express = require('express');
 const path = require('path');
 const storeService = require('./store-service')
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
+
+cloudinary.config({
+    cloud_name: 'dobubznlp',
+    api_key: '882644984493314',
+    api_secret: 'q3AZgF6MBSphr237rUvwBhIeSaY',
+    secure: true
+});
+
+const upload = multer(); 
 
 storeService.initialize()
     .then(() => {
@@ -49,14 +61,97 @@ app.get('/shop', (req, res) => {
 });
 
 app.get('/items', (req, res) => {
-    storeService.getAllItems()
-        .then(items => {
-            /* send data to the client */
-            res.json(items);
-        })
+    if (req.query.category) {
+        const category = parseInt(req.query.category);
+        if (isNaN(category) || category < 1 || category > 5) {
+            return res.status(400).json({ message: "Invalid category value" });
+        }
+        storeService.getItemsByCategory(category)
+            .then(items => res.json(items))
+            .catch(err => res.status(404).json({ message: err }));
+    } 
+    else if (req.query.minDate) {
+        if (!storeService.isValidDate(req.query.minDate)) {
+            return res.status(400).json({ message: "Invalid date format (Use YYYY-MM-DD)" });
+        }
+        storeService.getItemsByMinDate(req.query.minDate)
+            .then(items => res.json(items))
+            .catch(err => res.status(404).json({ message: err }));
+    } 
+    else {
+        storeService.getAllItems()
+            .then(items => {
+                /* send data to the client */
+                res.json(items);
+            })
+            .catch(err => {
+                /* return err message in the format: {message: err} */
+                res.status(500).json({ message: err });
+            });
+    }
+});
+
+app.get('/items/add', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/addItem.html'));
+});
+
+app.post('/items/add', upload.single("featureImage"), function (req, res, next) {
+    if(req.file) {
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processItem(uploaded.url);
+        });
+    }
+    else {
+        processItem("");
+    }
+     
+    function processItem(imageUrl){
+        req.body.featureImage = imageUrl;
+    
+        // TODO: Process the req.body and add it as a new Item before redirecting to /items
+        req.body.published = req.body.published ? true : false;
+            
+        storeService.addItem(req.body)
+            .then(() => resolve())
+            .catch(err => res.status(500).json({ message: err }));
+    } 
+});
+
+app.get('/item/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    
+    if (isNaN(itemId) || itemId < 1) {
+        return res.status(400).json({ message: "Invalid item ID" });
+    }
+
+    storeService.getItemById(itemId)
+        .then(item => res.json(item))
         .catch(err => {
-            /* return err message in the format: {message: err} */
-            res.status(500).json({ message: err });
+            if (err === "no results returned") {
+                res.status(404).json({ message: err });
+            }
         });
 });
 
--- store-service.js
+++ store-service.js
@@ -37,9 +37,61 @@ function getCategories() {
         : Promise.reject("no results returned");
 }
 
+function addItem(itemData) {
+    return new Promise((resolve) => {
+        itemData.published = itemData.published !== undefined ? true : false;
+        
+        itemData.id = items.length + 1;
+        
+        items.push(itemData);
+        
+        resolve(itemData);
+    });
+}
+
+function getItemsByCategory(category) {
+    return new Promise((resolve, reject) => {
+        const filtered = items.filter(item => item.category == category);
+        filtered.length > 0 
+            ? resolve(filtered)
+            : reject("no results returned");
+    });
+}
+
+function isValidDate(dateString) {
+    const regEx = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateString.match(regEx)) return false;
+    const d = new Date(dateString);
+    const dNum = d.getTime();
+    if (!dNum && dNum !== 0) return false;
+    return d.toISOString().slice(0,10) === dateString;
+}
+
+function getItemsByMinDate(minDateStr) {
+    return new Promise((resolve, reject) => {
+        const minDate = new Date(minDateStr);
+        const filtered = items.filter(item => new Date(item.postDate) >= minDate);
+        filtered.length > 0 
+            ? resolve(filtered)
+            : reject("no results returned");
+    });
+}
+
+function getItemById(id) {
+    return new Promise((resolve, reject) => {
+        const item = items.find(item => item.id === id);
+        item ? resolve(item) : reject("no results returned");
+    });
+}
+
 module.exports = {
     initialize,
     getAllItems,
     getPublishedItems,
-    getCategories
+    getCategories,
+    addItem,
+    getItemsByCategory, 
+    isValidDate,
+    getItemsByMinDate, 
+    getItemById
 };
